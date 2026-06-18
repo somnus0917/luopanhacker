6ae7dab930b61853235b536752ad1978706d2c69
somnus from macos <somnus0917chen@hotmail.com>
2026-06-18 09:45:34 +0800

    Fix cron browser environment

--- docker/cron_scrape.sh
+++ docker/cron_scrape.sh
@@ -6,6 +6,8 @@ cd /app
 echo "[$(date '+%Y-%m-%d %H:%M:%S')] start scheduled compass scrape"
 
 PYTHON_BIN="${PYTHON_BIN:-/usr/local/bin/python}"
+export CHROMIUM_EXECUTABLE_PATH="${CHROMIUM_EXECUTABLE_PATH:-/usr/bin/chromium}"
+export DISPLAY="${DISPLAY:-:99}"
 
 PYTHONUNBUFFERED=1 "$PYTHON_BIN" scheduler_run.py \
   --login-timeout-minutes 30
